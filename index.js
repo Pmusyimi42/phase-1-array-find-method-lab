fe7c0c41081d2779c6fe374436591d0ecaa85ffe
patrick kisyula <pmusyimi42@gmail.com>
2022-12-21 05:22:42 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    { year: "2012", result: "W"},
+    { year: "2011", result: "N/A"},
+    { year: "2010", result: "L"},
+    //...
+  ]
+const superbowlWin = (array1) => {
+  const win = array1.find((element)=>{
+    return element.result === "W"
+  })
+  if (typeof win === "object") {
+    return win.year
+  }
+};
